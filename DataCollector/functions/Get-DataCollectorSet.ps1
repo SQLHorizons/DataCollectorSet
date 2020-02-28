ffe9378bf23a078ffdaf1cd8f4a2a82ec19c4c09
Paul Maxfield <paul.dmax@sqlhorizons.com>
2020-02-28 17:28:09 +0000

    update

--- DataCollector/functions/Get-DataCollectorSet.ps1
+++ DataCollector/functions/Get-DataCollectorSet.ps1
@@ -21,9 +21,14 @@
     Process {
         Try {
             ##  query for data collector set.
-            $DataCollectorSet.Query($Name,$ServerNam)
+            $DataCollectorSet.Query($Name,$ServerName)
         }
         Catch [System.Exception] {
+            if ( 80300002 -eq $("{0:X4}" -f $($PSItem[0].Exception.ErrorCode) ) ) {
+                $DataCollectorSet = $false;
+                Return
+            }
+
             Write-Information  "Error at line: $(($PSItem[0].InvocationInfo.line).Trim())"
             $PSCmdlet.ThrowTerminatingError($PSItem)
         }
